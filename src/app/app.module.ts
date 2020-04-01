15deb5eacd863f79d1e2a6a649752e39afbbe5c0
kavya.pl <kavya.pl@gmail.com>
2020-04-01 07:34:32 +0400

    routing first class

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,71 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { ServicesComponent } from './services/services.component';
+import { CloudComponent } from './services/cloud/cloud.component';
+import { WebdevComponent } from './services/webdev/webdev.component';
+import { MobdevComponent } from './services/mobdev/mobdev.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutusComponent,
+    ServicesComponent,
+    CloudComponent,
+    WebdevComponent,
+    MobdevComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'mobdev',
+        component: MobdevComponent
+      },
+      {
+        path: 'aboutus',
+        component: AboutusComponent
+      },
+      {
+        path: 'services',
+        component: ServicesComponent,
+        children: [
+          {
+            path: '',
+            redirectTo: 'webdev',
+            pathMatch: 'full'
+          },
+          {
+            path: 'webdev',
+            component: WebdevComponent
+          },
+          {
+            path: 'mobdev',
+            component: MobdevComponent
+          },
+          {
+            path: 'cloud',
+            component: CloudComponent
+          }
+        ]
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/services/mobdev/mobdev.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mobdev',
+  templateUrl: './mobdev.component.html',
+  styleUrls: ['./mobdev.component.css']
+})
+export class MobdevComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
